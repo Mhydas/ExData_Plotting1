aec3cae7f98394ed75f5f86cde75dd076c4d95e4
Eric Shure <eric@ericshure.com>
2014-07-11 06:37:49 -0700

    Fixed res for plot 4 and it now seems to match

--- plot4.R
+++ plot4.R
@@ -1,6 +1,6 @@
 
-par(mfrow = c(2,2), mar = c(7,9,2,4))
-
+# par(mfrow = c(2,2), mar = c(6,3,3,3))
+par(mfrow = c(2,2))
 plot(electric$Date,electric$Global_active_power, type = 'l',
      ylab = 'Global Active Power',xlab = '')
 
@@ -17,16 +17,16 @@ lines(electric$Date,electric$Sub_metering_2, type = 'l', col = 'red')
 # 
 lines(electric$Date,electric$Sub_metering_3, type = 'l', col = 'blue')
 
-legend('topright', legend = names(electric[7:9]),lwd = 1, col = c('black','red','blue'))
+legend('topright', legend = names(electric[7:9]),lwd = 1, col = c('black','red','blue'), bty = 'n')
 
 plot(electric$Date, electric$Global_reactive_power, type = 'l',
      ylab = 'Global_reactive_power', xlab = 'datetime')
 
 # Open PNG device and create image file with a size of 480x480 px.
-png('plot4.png', width = 480, height = 480, units = 'px')
+png('plot4.png', width = 480, height = 480, units = 'px', res = 70)
 
-par(mfrow = c(2,2), mar = c(3,1,1,1))
-
+# par(mfrow = c(2,2), mar = c(2.1,2.1,2.1,2.1))
+par(mfrow = c(2,2))
 plot(electric$Date,electric$Global_active_power, type = 'l',
      ylab = 'Global Active Power',xlab = '')
 
@@ -43,7 +43,7 @@ lines(electric$Date,electric$Sub_metering_2, type = 'l', col = 'red')
 # 
 lines(electric$Date,electric$Sub_metering_3, type = 'l', col = 'blue')
 
-legend('topright', legend = names(electric[7:9]),lwd = 1, col = c('black','red','blue'))
+legend('topright', legend = names(electric[7:9]),lwd = 1, col = c('black','red','blue'), bty = 'n')
 
 plot(electric$Date, electric$Global_reactive_power, type = 'l',
      ylab = 'Global_reactive_power', xlab = 'datetime')
